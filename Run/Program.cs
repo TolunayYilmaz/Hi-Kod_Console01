70adf8068e88eec3cfe0636bfe51e3d637bc477d
Tolunay Yılmaz <tolunayyilmaz10@outlook.com>
2022-12-06 17:50:08 +0300

    Proje dosyası ekle.

---
+++ Run/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Run
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Write your Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Write your Surname");
+            string Surname= Console.ReadLine();
+            Console.WriteLine("Hello " + Name + " " + Surname);
+      
+
+        }
+    }
+}
